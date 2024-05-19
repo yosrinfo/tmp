66629079740ebb56bfa1a1d4bce8b42144ef40f5
yosriyahyaoui <mohamedyosriyahyaoui@gmail.com>
2024-05-19 14:40:17 +0200

    deployment 19052024

--- start/start.sh
+++ start/start.sh
@@ -4,15 +4,4 @@ CONFIG_LOCATION="../config/application.yml"
 JAR_FILE="../build/app-service.jar"
 PORT=8080
 
-Nohup java -jar -Dspring.config.location="$CONFIG_LOCATION" "$JAR_FILE" &
-
-sleep 5
-
-PID=$(lsof -ti:$PORT)
-
-if [ -z "$PID" ]; then
-    echo "Aucun processus n'utilise le port $PORT."
-else
-    echo "Le processus avec le PID $PID utilise le port $PORT. Il va être terminé."
-    kill $PID
-fi
+nohup java -jar -Dspring.config.location="$CONFIG_LOCATION" "$JAR_FILE"
